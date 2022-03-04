d3d19aa9d3b8384bdcff5229c5f0a8ed8feb7b8a
iremumut <irem2000.nur@gmail.com>
2022-03-04 18:43:52 +0300

    setup reducer

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { todoAdded } from "./todosSlice";
 
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+
+  const [text,setText] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(todoAdded(text));
+    setText("");
+  }
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>add todo</label>
+          <input type="text" onChange={handleChange} value={text}/>
+        </p>
+        <input type="submit" onClick={handleSubmit}/>
+      </form>
+      <p>Form text: {text}</p>
+    </div>
+  );
 }
 
-export default CreateTodo;
+export default CreateTodo; 
